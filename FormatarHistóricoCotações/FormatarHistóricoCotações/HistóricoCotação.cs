21a2cc1a054be4baa6ba93b7d68caf68a9c217a1
Erasmo de Melo Gusmão <erasmo.gusmao@gmail.com>
2015-11-26 13:29:26 -0300

    Tratar valores das ações
    - Correção do string do tamanho das cotações lido do arquivo original;
    - Criação de variáveis decimal para os valores das cotações

--- FormatarHistóricoCotações/FormatarHistóricoCotações/HistóricoCotação.cs
+++ FormatarHistóricoCotações/FormatarHistóricoCotações/HistóricoCotação.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Globalization;
+using System.Collections;
 
 namespace FormatarHistóricoCotações
 {
@@ -66,14 +67,25 @@ namespace FormatarHistóricoCotações
                             case "01":
                                 DateTime DataPregão = DateTime.ParseExact(LinhaDoArquivo.Substring(2,8), "yyyymmdd", DateTimeFormatInfo.InvariantInfo);    //Data do pregão
                                 DateTime DataVencimentoOpções = DateTime.ParseExact(LinhaDoArquivo.Substring(202,8), "yyyymmdd", DateTimeFormatInfo.InvariantInfo);    //Data do vencimento de Opções
-                                
+
+                                //Tratamento dos valores de cotações lidos
+                                decimal PreçoAbertura = decimal.Parse(LinhaDoArquivo.Substring(56, 13))/100;
+                                decimal PreçoMáximo = decimal.Parse(LinhaDoArquivo.Substring(69, 13)) / 100;
+                                decimal PreçoMínimo = decimal.Parse(LinhaDoArquivo.Substring(82, 13)) / 100;
+                                decimal PreçoMédio = decimal.Parse(LinhaDoArquivo.Substring(95, 13)) / 100;
+                                decimal PreçoAnterior = decimal.Parse(LinhaDoArquivo.Substring(108, 13)) / 100;
+                                decimal PreçoMelhorCompra = decimal.Parse(LinhaDoArquivo.Substring(121, 13)) / 100;
+                                decimal PreçoMelhorVenda = decimal.Parse(LinhaDoArquivo.Substring(134, 13)) / 100;
+                                decimal VolumeTotalNegociado = decimal.Parse(LinhaDoArquivo.Substring(170, 18)) / 100;
+                                decimal PreçoExercício = decimal.Parse(LinhaDoArquivo.Substring(188, 13)) / 100;
+
                                 writer.WriteLine(LinhaDoArquivo.Substring(0, 2) + "\t" + DataPregão.ToString("dd/mm/yyyy") + "\t" + LinhaDoArquivo.Substring(10, 2)
                             + "\t" + LinhaDoArquivo.Substring(12, 12) + "\t" + LinhaDoArquivo.Substring(24, 3) + "\t" + LinhaDoArquivo.Substring(27, 12)
                             + "\t" + LinhaDoArquivo.Substring(39, 10) + "\t" + LinhaDoArquivo.Substring(49, 3) + "\t" + LinhaDoArquivo.Substring(52, 4)
-                            + "\t" + LinhaDoArquivo.Substring(56, 11) + "\t" + LinhaDoArquivo.Substring(69, 11) + "\t" + LinhaDoArquivo.Substring(82, 11)
-                            + "\t" + LinhaDoArquivo.Substring(95, 11) + "\t" + LinhaDoArquivo.Substring(108, 11) + "\t" + LinhaDoArquivo.Substring(121, 11)
-                            + "\t" + LinhaDoArquivo.Substring(134, 11) + "\t" + LinhaDoArquivo.Substring(147, 5) + "\t" + LinhaDoArquivo.Substring(152, 18)
-                            + "\t" + LinhaDoArquivo.Substring(170, 16) + "\t" + LinhaDoArquivo.Substring(188, 11) + "\t" + LinhaDoArquivo.Substring(201, 1)
+                            + "\t" + PreçoAbertura.ToString() + "\t" + PreçoMáximo.ToString() + "\t" + PreçoMínimo.ToString()
+                            + "\t" + PreçoMédio.ToString() + "\t" + PreçoAnterior.ToString() + "\t" + PreçoMelhorCompra.ToString()
+                            + "\t" + PreçoMelhorVenda.ToString() + "\t" + LinhaDoArquivo.Substring(147, 5) + "\t" + LinhaDoArquivo.Substring(152, 18)
+                            + "\t" + VolumeTotalNegociado.ToString() + "\t" + PreçoExercício.ToString() + "\t" + LinhaDoArquivo.Substring(201, 1)
                             + "\t" + DataVencimentoOpções.ToString("dd/mm/yyyy") + "\t" + LinhaDoArquivo.Substring(210, 7) + "\t" + LinhaDoArquivo.Substring(217, 7)
                             + "\t" + LinhaDoArquivo.Substring(230, 12) + "\t" + LinhaDoArquivo.Substring(242, 3));
                                 break;
