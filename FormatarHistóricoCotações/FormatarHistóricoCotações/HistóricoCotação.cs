2b65d1fd06618c00cdf5065c7d400ccd25688aef
Erasmo de Melo Gusmão <erasmo.gusmao@gmail.com>
2015-11-27 08:26:37 -0300

    Tratamento de Exceções do método de formatação de arquivo
    - FormatException ( Arquivo com formato errado)
    - FileNotFoundException ( Arqruivo não encontrado)

--- FormatarHistóricoCotações/FormatarHistóricoCotações/HistóricoCotação.cs
+++ FormatarHistóricoCotações/FormatarHistóricoCotações/HistóricoCotação.cs
@@ -50,54 +50,70 @@ namespace FormatarHistóricoCotações
         }
 
         public void FormatarArquivoDeCotações() {
-            using (StreamReader reader = new StreamReader(@"C:\Users\Erasmo\Documents\GitHub\AnaliseMercado\FormatarHistóricoCotações\Arquivos de Testes\COTAHIST_A2015.txt"))
+
+            try
             {
-                using (StreamWriter writer = new StreamWriter(@"C:\Users\Erasmo\Documents\GitHub\AnaliseMercado\FormatarHistóricoCotações\Arquivos de Testes\CotaçãoFormatada.txt", false)) { 
-                
-                    while(!reader.EndOfStream)
+                using (StreamReader reader = new StreamReader(@"C:\Users\Erasmo\Documents\GitHub\AnaliseMercado\FormatarHistóricoCotações\Arquivos de Testes\DemoCotacoesHistoricas12022003.txt"))
+                {
+                    using (StreamWriter writer = new StreamWriter(@"C:\Users\Erasmo\Documents\GitHub\AnaliseMercado\FormatarHistóricoCotações\Arquivos de Testes\CotaçãoFormatada.txt", false))
                     {
-                        string LinhaDoArquivo = reader.ReadLine();
-                        switch (LinhaDoArquivo.Substring(0,2))
+
+                        try
                         {
-                            case "00":
-                                DateTime DataGeraçãoArquivo = DateTime.ParseExact(LinhaDoArquivo.Substring(23,8), "yyyymmdd", DateTimeFormatInfo.InvariantInfo);    //Converte um String em DateTime do cabeçalho
-                                writer.WriteLine(LinhaDoArquivo.Substring(0, 2) + "\t" + LinhaDoArquivo.Substring(2, 13) + "\t" + LinhaDoArquivo.Substring(15, 8) + "\t" + DataGeraçãoArquivo.ToString("dd/mm/yyyy") + "\t" + LinhaDoArquivo.Substring(31, 214));  //Escreve o Cabeçalho
-                                break;
+                            while (!reader.EndOfStream)
+                            {
+                                string LinhaDoArquivo = reader.ReadLine();
+                                switch (LinhaDoArquivo.Substring(0, 2))
+                                {
+                                    case "00":
+                                        DateTime DataGeraçãoArquivo = DateTime.ParseExact(LinhaDoArquivo.Substring(23, 8), "yyyymmdd", DateTimeFormatInfo.InvariantInfo);    //Converte um String em DateTime do cabeçalho
+                                        writer.WriteLine(LinhaDoArquivo.Substring(0, 2) + "\t" + LinhaDoArquivo.Substring(2, 13) + "\t" + LinhaDoArquivo.Substring(15, 8) + "\t" + DataGeraçãoArquivo.ToString("dd/mm/yyyy") + "\t" + LinhaDoArquivo.Substring(31, 214));  //Escreve o Cabeçalho
+                                        break;
 
-                            case "01":
-                                DateTime DataPregão = DateTime.ParseExact(LinhaDoArquivo.Substring(2,8), "yyyymmdd", DateTimeFormatInfo.InvariantInfo);    //Data do pregão
-                                DateTime DataVencimentoOpções = DateTime.ParseExact(LinhaDoArquivo.Substring(202,8), "yyyymmdd", DateTimeFormatInfo.InvariantInfo);    //Data do vencimento de Opções
+                                    case "01":
+                                        DateTime DataPregão = DateTime.ParseExact(LinhaDoArquivo.Substring(2, 8), "yyyymmdd", DateTimeFormatInfo.InvariantInfo);    //Data do pregão
+                                        DateTime DataVencimentoOpções = DateTime.ParseExact(LinhaDoArquivo.Substring(202, 8), "yyyymmdd", DateTimeFormatInfo.InvariantInfo);    //Data do vencimento de Opções
 
-                                //Tratamento dos valores de cotações lidos
-                                decimal PreçoAbertura = decimal.Parse(LinhaDoArquivo.Substring(56, 13))/100;
-                                decimal PreçoMáximo = decimal.Parse(LinhaDoArquivo.Substring(69, 13)) / 100;
-                                decimal PreçoMínimo = decimal.Parse(LinhaDoArquivo.Substring(82, 13)) / 100;
-                                decimal PreçoMédio = decimal.Parse(LinhaDoArquivo.Substring(95, 13)) / 100;
-                                decimal PreçoAnterior = decimal.Parse(LinhaDoArquivo.Substring(108, 13)) / 100;
-                                decimal PreçoMelhorCompra = decimal.Parse(LinhaDoArquivo.Substring(121, 13)) / 100;
-                                decimal PreçoMelhorVenda = decimal.Parse(LinhaDoArquivo.Substring(134, 13)) / 100;
-                                decimal VolumeTotalNegociado = decimal.Parse(LinhaDoArquivo.Substring(170, 18)) / 100;
-                                decimal PreçoExercício = decimal.Parse(LinhaDoArquivo.Substring(188, 13)) / 100;
+                                        //Tratamento dos valores de cotações lidos
+                                        decimal PreçoAbertura = decimal.Parse(LinhaDoArquivo.Substring(56, 13)) / 100;
+                                        decimal PreçoMáximo = decimal.Parse(LinhaDoArquivo.Substring(69, 13)) / 100;
+                                        decimal PreçoMínimo = decimal.Parse(LinhaDoArquivo.Substring(82, 13)) / 100;
+                                        decimal PreçoMédio = decimal.Parse(LinhaDoArquivo.Substring(95, 13)) / 100;
+                                        decimal PreçoAnterior = decimal.Parse(LinhaDoArquivo.Substring(108, 13)) / 100;
+                                        decimal PreçoMelhorCompra = decimal.Parse(LinhaDoArquivo.Substring(121, 13)) / 100;
+                                        decimal PreçoMelhorVenda = decimal.Parse(LinhaDoArquivo.Substring(134, 13)) / 100;
+                                        decimal VolumeTotalNegociado = decimal.Parse(LinhaDoArquivo.Substring(170, 18)) / 100;
+                                        decimal PreçoExercício = decimal.Parse(LinhaDoArquivo.Substring(188, 13)) / 100;
 
-                                writer.WriteLine(LinhaDoArquivo.Substring(0, 2) + "\t" + DataPregão.ToString("dd/mm/yyyy") + "\t" + LinhaDoArquivo.Substring(10, 2)
-                            + "\t" + LinhaDoArquivo.Substring(12, 12) + "\t" + LinhaDoArquivo.Substring(24, 3) + "\t" + LinhaDoArquivo.Substring(27, 12)
-                            + "\t" + LinhaDoArquivo.Substring(39, 10) + "\t" + LinhaDoArquivo.Substring(49, 3) + "\t" + LinhaDoArquivo.Substring(52, 4)
-                            + "\t" + PreçoAbertura.ToString() + "\t" + PreçoMáximo.ToString() + "\t" + PreçoMínimo.ToString()
-                            + "\t" + PreçoMédio.ToString() + "\t" + PreçoAnterior.ToString() + "\t" + PreçoMelhorCompra.ToString()
-                            + "\t" + PreçoMelhorVenda.ToString() + "\t" + LinhaDoArquivo.Substring(147, 5) + "\t" + LinhaDoArquivo.Substring(152, 18)
-                            + "\t" + VolumeTotalNegociado.ToString() + "\t" + PreçoExercício.ToString() + "\t" + LinhaDoArquivo.Substring(201, 1)
-                            + "\t" + DataVencimentoOpções.ToString("dd/mm/yyyy") + "\t" + LinhaDoArquivo.Substring(210, 7) + "\t" + LinhaDoArquivo.Substring(217, 7)
-                            + "\t" + LinhaDoArquivo.Substring(230, 12) + "\t" + LinhaDoArquivo.Substring(242, 3));
-                                break;
+                                        writer.WriteLine(LinhaDoArquivo.Substring(0, 2) + "\t" + DataPregão.ToString("dd/mm/yyyy") + "\t" + LinhaDoArquivo.Substring(10, 2)
+                                    + "\t" + LinhaDoArquivo.Substring(12, 12) + "\t" + LinhaDoArquivo.Substring(24, 3) + "\t" + LinhaDoArquivo.Substring(27, 12)
+                                    + "\t" + LinhaDoArquivo.Substring(39, 10) + "\t" + LinhaDoArquivo.Substring(49, 3) + "\t" + LinhaDoArquivo.Substring(52, 4)
+                                    + "\t" + PreçoAbertura.ToString() + "\t" + PreçoMáximo.ToString() + "\t" + PreçoMínimo.ToString()
+                                    + "\t" + PreçoMédio.ToString() + "\t" + PreçoAnterior.ToString() + "\t" + PreçoMelhorCompra.ToString()
+                                    + "\t" + PreçoMelhorVenda.ToString() + "\t" + LinhaDoArquivo.Substring(147, 5) + "\t" + LinhaDoArquivo.Substring(152, 18)
+                                    + "\t" + VolumeTotalNegociado.ToString() + "\t" + PreçoExercício.ToString() + "\t" + LinhaDoArquivo.Substring(201, 1)
+                                    + "\t" + DataVencimentoOpções.ToString("dd/mm/yyyy") + "\t" + LinhaDoArquivo.Substring(210, 7) + "\t" + LinhaDoArquivo.Substring(217, 7)
+                                    + "\t" + LinhaDoArquivo.Substring(230, 12) + "\t" + LinhaDoArquivo.Substring(242, 3));
+                                        break;
 
-                            case "99":
-                                DateTime DataGeraçãoArquivoFim = DateTime.ParseExact(LinhaDoArquivo.Substring(23,8), "yyyymmdd", DateTimeFormatInfo.InvariantInfo);    //Converte um String em DateTime do fim do arquivo
-                                writer.WriteLine(LinhaDoArquivo.Substring(0, 2) + "\t" + LinhaDoArquivo.Substring(2, 13) + "\t" + LinhaDoArquivo.Substring(15, 8) + "\t" + DataGeraçãoArquivoFim.ToString("dd/mm/yyyy") + "\t" + LinhaDoArquivo.Substring(31, 11)+"\t"+LinhaDoArquivo.Substring(42,203));  //Escreve o Cabeçalho
-                                break;
+                                    case "99":
+                                        DateTime DataGeraçãoArquivoFim = DateTime.ParseExact(LinhaDoArquivo.Substring(23, 8), "yyyymmdd", DateTimeFormatInfo.InvariantInfo);    //Converte um String em DateTime do fim do arquivo
+                                        writer.WriteLine(LinhaDoArquivo.Substring(0, 2) + "\t" + LinhaDoArquivo.Substring(2, 13) + "\t" + LinhaDoArquivo.Substring(15, 8) + "\t" + DataGeraçãoArquivoFim.ToString("dd/mm/yyyy") + "\t" + LinhaDoArquivo.Substring(31, 11) + "\t" + LinhaDoArquivo.Substring(42, 203));  //Escreve o Cabeçalho
+                                        break;
 
+                                }
+                            }
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Incapaz de ler arquivo com formato errado!", "Sua execução foi invalidada!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         }
                     }
                 }
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("ARQUIVO NÃO ENCONTRADO!","Sua execução foi invalidada!",MessageBoxButtons.OK,MessageBoxIcon.Error);
             }
         }
     }
